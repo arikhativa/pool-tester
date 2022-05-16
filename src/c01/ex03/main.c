8020569095ba020cc2f8fc2a43f6d283c4344a40
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-16 15:38:28 +0200

    ex03 done

---
+++ src/c01/ex03/main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void ft_div_mod(int a, int b, int *div, int *mod);
+
+int main()
+{
+	int a = 422;
+	int b = 10;
+	int div = 42;
+	int mod = 2;
+	int *ptr = NULL;
+
+	ft_div_mod(a, 0, &div, &mod);
+	ft_div_mod(a, b, &div, ptr);
+	ft_div_mod(a, b, ptr, &mod);
+	
+	ft_div_mod(a, b, &div, &mod);
+
+	printf("%d %d", div, mod);
+}
