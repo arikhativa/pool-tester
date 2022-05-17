b952c2e892e65787c485bc4a22ffb855f7bfd06a
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-17 10:24:32 +0200

    ex06 and ex07 done

--- src/c01/ex06/main.c
+++ src/c01/ex06/main.c
@@ -1,39 +1,15 @@
 
 #include <stdio.h>
 
-void	ft_rev_int_tab(int *tab, int size);
-
-void	print_tab(int *tab, int size)
-{
-	int	i;
-
-	i = -1;
-	while (++i < size)
-		printf("%d", tab[i]);
-	printf("\n");
-}
+int	ft_strlen(char *str);
 
 int main()
 {
-	int tab1[] = {1};
-	int tab2[] = {1, 2};
-	int tab3[] = {1, 2, 3};
-	int tab4[] = {1, 2, 3, 4};
-	int tab5[] = {1, 2, 3, 4, 5};
-	int	*ptr = NULL;
+	char *s1 = "1";
+	char *s2 = "";
+	char *s3 = "12345678901";
 
-	ft_rev_int_tab(ptr, 5);
-	ft_rev_int_tab(tab2, 0);
-
-	ft_rev_int_tab(tab1, 1);
-	ft_rev_int_tab(tab2, 2);
-	ft_rev_int_tab(tab3, 3);
-	ft_rev_int_tab(tab4, 4);
-	ft_rev_int_tab(tab5, 5);
-
-	print_tab(tab1, 1);
-	print_tab(tab2, 2);
-	print_tab(tab3, 3);
-	print_tab(tab4, 4);
-	print_tab(tab5, 5);
+	printf("%d\n", ft_strlen(s1));
+	printf("%d\n", ft_strlen(s2));
+	printf("%d\n", ft_strlen(s3));
 }
