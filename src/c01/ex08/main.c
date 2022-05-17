b4cca0ea7eac6b915422a83dc47c3acec3d57c2e
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-17 11:07:40 +0200

    ex08 done

---
+++ src/c01/ex08/main.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size);
+
+void	print_tab(int *tab, int size)
+{
+	int	i;
+
+	i = 0;
+	printf("%d", tab[i]);
+	while (++i < size)
+		printf(", %d", tab[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int tab1[] = {10};
+	int tab2[] = {10, 2, 3, 8, 9, 7, 4, 5, 0, 1, 6};
+	int tab3[] = {0, -22, 200000, 100, -1};
+	int tab4[] = {2147483647, -2147483648};
+	int	*ptr = NULL;
+
+	ft_sort_int_tab(ptr, 5);
+	ft_sort_int_tab(tab2, 0);
+
+	ft_sort_int_tab(tab1, 1);
+	ft_sort_int_tab(tab2, 11);
+	ft_sort_int_tab(tab3, 5);
+	ft_sort_int_tab(tab4, 2);
+
+	print_tab(tab1, 1);
+	print_tab(tab2, 11);
+	print_tab(tab3, 5);
+	print_tab(tab4, 2);
+}
