fa65179d8cac00ba89d185c0031903a14fcd89ce
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-19 12:31:59 +0200

    ex03 done

--- src/c02/ex03/main.c
+++ src/c02/ex03/main.c
@@ -1,23 +1,23 @@
 
 #include <stdio.h>
 
-int ft_str_is_alpha(char *str);
+int ft_str_is_numeric(char *str);
 
 int	main()
 {
-	char *s1 = "string";
-	char *s2 = "string2";
+	char *s1 = "12345";
+	char *s2 = "123s";
 	char *s3 = "";
 	char s4[] = {1, 92, 64, 30, '\0'};
 	int ret;
 
-	ret = ft_str_is_alpha(s1);
+	ret = ft_str_is_numeric(s1);
 	printf("%d\n", ret);
-	ret = ft_str_is_alpha(s2);
+	ret = ft_str_is_numeric(s2);
 	printf("%d\n", ret);
-	ret = ft_str_is_alpha(s3);
+	ret = ft_str_is_numeric(s3);
 	printf("%d\n", ret);
-	ret = ft_str_is_alpha(s4);
+	ret = ft_str_is_numeric(s4);
 	printf("%d\n", ret);
 
 	return (0);
