09814c8626fcf63f9727158eaed2b05f1cc2cbef
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-23 14:11:11 +0200

    ex04

--- src/c03/ex04/main.c
+++ src/c03/ex04/main.c
@@ -8,9 +8,14 @@ int	main()
 	char haystack[] = "123abc123";
 	char needle[] = "abc";
 	char *ret;
+	int	is_same = 0;
 
-	// ret = ft_strstr(haystack, "");
-	// printf("%p\n", ret);
+	ret = ft_strstr(haystack, "");
+	is_same = (ret == haystack);
+	if (!is_same)
+		printf("User didn't return haystack\n");
+	ret = ft_strstr(haystack, "NO");
+	printf("%p\n", ret);
 	ret = ft_strstr(haystack, needle);
 	printf("%s\n", ret);
 
