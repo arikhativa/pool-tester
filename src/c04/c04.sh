7a1ed0280a7edb0006fcb16225e80b9f6c68a299
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-24 11:44:15 +0200

    ex01

--- src/c04/c04.sh
+++ src/c04/c04.sh
@@ -2,7 +2,7 @@
 
 test_c04()
 {
-	local TESTS_NAMES=(ft_strlen )
+	local TESTS_NAMES=(ft_strlen ft_putstr)
 	local NUM_OF_FILES_PER_TEST=(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
 
 	generic_project_test "c04" $TESTS_NAMES $NUM_OF_FILES_PER_TEST
---
+++ src/c04/ex01/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+void ft_putstr(char *str);
+
+int	main(void)
+{
+	char *s0 = "12345\n";
+	char *s1 = "";
+	char *s2 = "Veryyyy Logggg Starrrrrrrrrrrr                     asdasdasdssyyyy Logggg Starrrrrrrrrrrr                     asdasdasdss";
+
+	ft_putstr(s0);
+	ft_putstr(s1);
+	ft_putstr(s2);
+}
