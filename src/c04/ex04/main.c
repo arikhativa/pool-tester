287bc56f6792f7602938b1ecca2d0699a8f511d1
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-24 16:17:05 +0200

    ex04 done

--- src/c04/ex04/main.c
+++ src/c04/ex04/main.c
@@ -8,8 +8,14 @@ int	main(void)
 {
 	char *decimal = "0123456789";
 	char *binary = "01";
-	char *octal = "poneyvif";
+	char *octal = "01234567";
 	char *hex = "0123456789ABCDEF";
+	char *pony = "pony";
+
+	ft_putnbr_base(42, "");
+	ft_putnbr_base(42, "123+");
+	ft_putnbr_base(42, "123-");
+	ft_putnbr_base(42, "aa");
 
 	write(1, "decimal\n", 8);
 	ft_putnbr_base(42, decimal); write(1, "\n", 1);
@@ -38,6 +44,17 @@ int	main(void)
 	ft_putnbr_base(255, hex); write(1, "\n", 1);
 	ft_putnbr_base(2147483647, hex); write(1, "\n", 1);
 	ft_putnbr_base(-2147483648, hex); write(1, "\n", 1);
+	write(1, "\n", 1);
+
+	write(1, "pony\n", 5);
+	ft_putnbr_base(0, pony); write(1, "\n", 1);
+	ft_putnbr_base(10, pony); write(1, "\n", 1);
+	ft_putnbr_base(-65, pony); write(1, "\n", 1);
+	ft_putnbr_base(100, pony); write(1, "\n", 1);
+	write(1, "\n", 1);
+
+	write(1, "long\n", 5);
+	ft_putnbr_base(100, "0123456789abcdefghijklmnopqrstuvwxyz"); write(1, "\n", 1);
 
 	return (0);
 }
