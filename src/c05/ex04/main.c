8c361712080107a31500654af199f3e7f57eb635
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-25 12:26:20 +0200

    ex04

---
+++ src/c05/ex04/main.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int ft_fibonacci(int index);
+
+int	main(void)
+{
+	printf("%d ", ft_fibonacci(-1));
+	printf("%d ", ft_fibonacci(0));
+	printf("%d ", ft_fibonacci(1));
+	printf("%d ", ft_fibonacci(2));
+	printf("%d ", ft_fibonacci(3));
+	printf("%d ", ft_fibonacci(4));
+	printf("%d ", ft_fibonacci(5));
+	printf("%d ", ft_fibonacci(6));
+	printf("%d ", ft_fibonacci(7));
+	printf("%d ", ft_fibonacci(8));
+	printf("%d ", ft_fibonacci(12));
+}
