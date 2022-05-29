f41f2fc7ac3c17d73e12467c5921d3632efff928
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-29 17:55:40 +0200

    ex01

--- src/c07/c07.sh
+++ src/c07/c07.sh
@@ -2,7 +2,7 @@
 
 test_c07()
 {
-	local TESTS_NAMES=(ft_strdup)
+	local TESTS_NAMES=(ft_strdup ft_range)
 	local NUM_OF_FILES_PER_TEST=(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
 
 	generic_project_test "c07" $TESTS_NAMES $NUM_OF_FILES_PER_TEST
---
+++ src/c07/ex01/main.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int *ft_range(int min, int max);
+
+void	print_arr(int *arr, int size)
+{
+	int i = 0;
+	while (i < size)
+	{
+		printf("%d ", arr[i]);
+		++i;
+	}
+	printf("\n");
+}
+
+int	main(void)
+{
+	int *ret;
+
+	ret = ft_range(0, 3);
+	print_arr(ret, 3);
+	free(ret);
+
+	ret = ft_range(-10, -5);
+	print_arr(ret, 5);
+	free(ret);
+
+	ret = ft_range(-2147483648, -2147483646);
+	print_arr(ret, 2);
+	free(ret);
+
+	ret = ft_range(2147483645, 2147483647);
+	print_arr(ret, 2);
+	free(ret);
+
+	ret = ft_range(-2, 3);
+	print_arr(ret, 5);
+	free(ret);
+}
