2295e0784e3cb7890fd9993896cb0ea0d9cee7d3
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-31 15:58:54 +0200

    mid ex04

---
+++ src/c08/ex04/ft_stock_str.h
@@ -0,0 +1,11 @@
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+void set_point(t_point *point)
+{
+	point->x = 42;
+	point->y = 21;
+}
+
+#endif
---
+++ src/c08/ex04/main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include "ft_stock_str.h"
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av);
+
+int main(void)
+{
+	return (0);
+}
