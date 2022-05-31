3e6d64a4f4a3667d019d8a1e84c11af98443ef0b
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-31 15:47:54 +0200

    ex02

--- src/c08/c08.sh
+++ src/c08/c08.sh
@@ -2,7 +2,7 @@
 
 test_c08()
 {
-	local TESTS_NAMES=(ft ft_boolean)
+	local TESTS_NAMES=(ft ft_boolean ft_abs)
 	local NUM_OF_FILES_PER_TEST=(1 1 1 1 1)
 
 	generic_project_test "c08" $TESTS_NAMES $NUM_OF_FILES_PER_TEST compile_tests_c08 check_norm_c08
---
+++ src/c08/ex02/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+#include "ft_abs.h"
+
+int main(void)
+{
+	printf("%d ", ABS(-1));
+	printf("%d ", ABS(-1 * 2));
+	printf("%d ", ABS(-1 -2));
+	printf("%d ", ABS(-3-2));
+
+	return (0);
+}
